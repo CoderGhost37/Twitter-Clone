acd20dd79d5d4c234e24f8a6fc5950049e9ba22b
Ghost <ghostridergamer37@gmail.com>
2022-07-30 16:52:12 +0530

    eslint configured

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from 'next/head';
 
-export default function Home() {
+const Home = () => {
+  console.log('Home');
   return (
     <div>
       <Head>
         <title>Twitter</title>
         <link rel="icon" href="/twitter-logo.png" />
       </Head>
-      <h1 className='text-blue-500 font-bold'>Let us Begin</h1>
+      <h1 className="text-blue-500 font-bold">Let us Begin</h1>
     </div>
-  )
-}
+  );
+};
+
+export default Home;
